4d788e90a107234af606d7836228d537d7f97b7d
maobonhaha <maobonhaha@gmail.com>
2024-05-11 22:30:00 +0800

    commit-7

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -128,6 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 
 # Heroku settings
-# import django_heroku
-
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
